89965e4e0d4819dcf997793fe4a1c49a718746b5
universaliensis <43149059+universaliensis@users.noreply.github.com>
2021-03-28 15:15:36 +0200

    Add files via upload

--- greeting-clock-card.js
+++ greeting-clock-card.js
@@ -1,4 +1,49 @@
-class GreetingClockCard extends HTMLElement {
+var time_str;
+
+const LitElement = window.LitElement
+  || Object.getPrototypeOf(customElements.get("home-assistant") || customElements.get("hui-view"))
+const { html, css } = LitElement.prototype;
+
+
+class GreetingClockCard extends LitElement {
+  static get properties() {
+    return {
+      _hass: { type: Object },
+      config: {}
+    };
+  }
+
+  set hass(hass) {
+    this._hass = hass;
+    this.startTime();
+    setInterval(this.startTime.bind(this), 250);
+
+  }
+
+  render() {
+    return html`
+      <ha-card
+        ?night=${this.isNight}
+        style="--day-color: #45aaf2; --night-color: #a55eea; --text-color: var(--text-dark-color);">
+        <div class="calendar__icon">
+        </div>
+        <div class="clock__info">
+        <div class="clock__info__title">
+        ${time_str}
+        </div>
+        <div class="clock__info__state">
+        ${this._hass.states['sensor.sidebar_day'].state} den ${this._hass.states['sensor.sidebar_date'].state}
+        </div>
+        </div>
+        <div class="clock__info">
+
+        </div>
+      </ha-card>
+    `;
+  }
+
+
+
 
   addZero(i){
     if (i < 10){
@@ -6,47 +51,26 @@ class GreetingClockCard extends HTMLElement {
     }
     return i;
   }
-  
+
   startTime() {
     var today = new Date(),
         h = today.getHours(),
         m = today.getMinutes(),
-        s = today.getSeconds(),
+        //s = today.getSeconds(),
         p = ( h < 12 ) ? "AM" : "PM";
 
     m = this.addZero(m);
-    s = this.addZero(s);
+    //s = this.addZero(s);
 
-    let time_str =  (this.config.use_military ? h % 12 : h ) +
+    time_str =  (this.config.use_military ? h % 12 : h ) +
                     ":" +
-                    m +
-                    (this.config.hide_seconds ? "" : ":" + s ) +
-                    (this.config.use_military ? " " + p : " ");
+                    m;
 
-    this.content.innerHTML = time_str;
+    //this.content.innerHTML = time_str;
 
   }
 
-  set hass(hass) {
-    if (!this.content) {
-      const card = document.createElement('ha-card');
-      card.header = 'TEST CARD 2';
-      this.content = document.createElement('div');
-      this.content.style.padding = '0 16px 16px';
-      card.appendChild(this.content);
-      this.appendChild(card);
-    }
 
-    this.startTime();
-    setInterval(this.startTime.bind(this), 250);
-
-  //  this.content.innerHTML = `
-  //    The state of ${entityId} is ${stateStr}!
-  //    <br><br>
-  //    <img src="http://via.placeholder.com/350x150">
-  //  `;
-
-  }
 
   setConfig(config) {
     if (!config.entity) {
@@ -60,6 +84,66 @@ class GreetingClockCard extends HTMLElement {
   getCardSize() {
     return 3;
   }
+
+  get isNight() {
+    return this._hass.states['sun.sun']
+      ? this._hass.states['sun.sun'].state === 'below_horizon'
+      : false;
+  }
+
+  static get styles() {
+    return css`
+    ha-card {
+      display: flex;
+      flex-flow: row;
+      align-items: center;
+      padding: 10px;
+      background: #45aaf2;
+      font-weight: var(--swc-font-weight, 500);
+      transition: background 1s;
+      cursor: pointer;
+      color: var(--text-color);
+    }
+    ha-card[night] {
+      background: #a55eea;
+    }
+    .clock_info {
+      display: flex;
+      align-items:flex-start;
+      align-content:flex-start;
+      flex-flow: column;
+      justify-content: space-between;
+      min-height: 100px;
+      min-width: 100px;
+    }
+    .clock__info__title {
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      font-size: 50px;
+      line-height: 1;
+      padding:5px;
+    }
+    .clock__info__state {
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      font-size: 25px;
+      padding:5px;
+    }
+    .calendar__icon {
+      height: 100px;
+      background-size: contain;
+      background-repeat: no-repeat;
+      flex: 0 0 0px;
+      color: white;
+      margin-right: 16px;
+    }
+    `;
+  }
+
+
+
 }
 
 customElements.define('greeting-clock-card', GreetingClockCard);
